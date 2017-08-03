71b47e62b922ba97702bf7c2d7e95a5a9e3b148c
Eric Abraham <eric.abraham@mca.christuniversity.in>
2017-08-03 23:50:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,3 +23,19 @@ button.onclick = function(){
 request.open('GET', 'http://ericabrahamea.imad.hasura-app.io/counter', true);
 request.send(null);
 };
+
+//submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+    //make http request to server and submit name
+    
+    //capture a list of names and render it as a list
+    var names = ['name1','name2','name3'];
+    var list = '';
+    for (var i=0;i<names.length; i++){
+        list+='<li>' + names[i] + '</li>';
+    }
+    var ul = doucment.getElementById('namelist');
+};
