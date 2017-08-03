3590f7aeaff99be66628d80b9efada8861416426
Eric Abraham <eric.abraham@mca.christuniversity.in>
2017-08-04 00:29:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -48,7 +48,7 @@ submit.onclick = function(){
     };
     
     //Make the request
-    var nameInput = document.getElementById('name');
+var nameInput = document.getElementById('name');
 var name = nameInput.value;
 request.open('GET', 'http://ericabrahamea.imad.hasura-app.io/submit-name?name=' +name, true);
 request.send(null);
