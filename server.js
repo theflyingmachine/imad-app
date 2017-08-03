db11ceb1079a9c07bff963c2afc90be572d2e16f
Eric Abraham <eric.abraham@mca.christuniversity.in>
2017-08-03 23:19:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,6 +98,10 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/:artName', function (req, res) {
     var artName = req.params.artName;
   res.send(createTemplate(article[artName]));
