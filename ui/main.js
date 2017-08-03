b999c2a505249c2f6e3397a5c68ce70f11aeeff4
Eric Abraham <eric.abraham@mca.christuniversity.in>
2017-08-03 23:25:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,22 @@
 //counter code
 var button = document.getElementById('counter');
 var counter = 0;
-alert("I am an alert box!");
+
 button.onclick = function(){
     //make a request to the counter endpoint
+    var request = new XMLHttpRequest();
     
     //capture the responce and store it into the variable
+    request.onreadystatechange = function(){
+        if (request.readyState === XMLHttpRequest.DONE){
+            //take some action
+            if (request.status ===200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    }
     
     //render the varianle in the correct span
     counter = counter + 1;
