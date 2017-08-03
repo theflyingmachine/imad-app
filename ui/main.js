7f872c37fc74a9915a57017e8e4cddee9e69aff3
Eric Abraham <eric.abraham@mca.christuniversity.in>
2017-08-04 00:11:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -35,7 +35,6 @@ submit.onclick = function(){
     var list = '';
     for (var i=0;i<names.length; i++){
         list+='<li>' + names[i] + '</li>';
-      //  alert("Httpdeo");
     }
     var ul = document.getElementById('namelist');
     ul.innerHTML = list;
