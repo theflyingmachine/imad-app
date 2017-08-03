ea5d2da3adac2664cd5a045cce0cc081dea0c7e4
Eric Abraham <eric.abraham@mca.christuniversity.in>
2017-08-03 22:53:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,14 @@
-console.log('Loaded!');
+//counter code
+var button = document.getElementById('counter');
+var counter = 0;
+
+button.onclick = function(){
+    //make a request to the counter endpoint
+    
+    //capture the responce and store it into the variable
+    
+    //render the varianle in the correct span
+    counter=counter+1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+};
