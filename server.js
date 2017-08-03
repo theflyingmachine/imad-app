3ede0bc446d2cf6e5c5a070e834995df5243b14b
Eric Abraham <eric.abraham@mca.christuniversity.in>
2017-08-04 00:14:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,9 +104,9 @@ app.get('/ui/main.js', function (req, res) {
 
 
 var names = [];
-app.get('/submit-name/:name', function (req, res) {
+app.get('/submit-name', function (req, res) {
   //get the name from the request
-  var name = req.params.name;
+  var name = req.query.name;
   names.push(name);
   //JSON: javascript obkect notation
   res.send(JSON.stringify(names));
