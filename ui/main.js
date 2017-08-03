b7bab9834425dbab7facf2ee17ecfef55685854a
Eric Abraham <eric.abraham@mca.christuniversity.in>
2017-08-04 00:32:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -37,7 +37,7 @@ submit.onclick = function(){
             if (request.status ===200){
                     var names = request.responseText;
                     names = JSON.paser(names);
-                    var list = '';
+                    var list = "";
                     for (var i=0;i<names.length; i++){
                         list+='<li>' + names[i] + '</li>';
                     }
