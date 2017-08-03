61b87494bf893175e1385015db7a02e13b1e0f16
Eric Abraham <eric.abraham@mca.christuniversity.in>
2017-08-04 00:03:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,13 +26,13 @@ request.send(null);
 
 //submit name
 var nameInput = document.getElementById('name');
-var names = nameInput.value;
+var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     //make http request to server and submit name
     alert("Httpdeo");
     //capture a list of names and render it as a list
-    var names = ['name1','name2','name3'];
+    var names = ['name1', 'name2', 'name3', 'name4'];
     var list = '';
     for (var i=0;i<names.length; i++){
         list+='<li>' + names[i] + '</li>';
