f714e04250ce436463b064a9bad703cef5844a02
Eric Abraham <eric.abraham@mca.christuniversity.in>
2017-08-18 17:45:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ pool.query('SELECT * FROM USER', function(err,result){
 function hash(input, salt){
     // function to create a hash
 var  hashed = crypto.pbkdf2Sync('secret', 'salt', 10000, 512, 'sha512');
-return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$');
+return ["pbkdf2", "10000", salt, input, hashed.toString('hex')].join('$');
 }
 
 
